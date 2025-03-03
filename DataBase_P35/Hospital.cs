b244170cde5dd967f182241ecce666934dcc9cd7
svitlyi <svitlyi_a@itstep.academy>
2025-03-03 19:48:04 +0200

    Add project files.

---
+++ DataBase_P35/Hospital.cs
@@ -0,0 +1,34 @@
+﻿using Hospital;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    class Doctor
+    {
+
+    }
+}
+
+/*
+    1. Створити клас Doctor, в якому зберігається
+        інформація про лікаря
+    2. Змінити метод GetDoctors таким чином, 
+        щоб він не виводив таблицю з лікарями, а
+        повертав список об`єктів класу Doctor.
+        Список лікарів — List<Doctor>
+    3. Створити функцію ShowDoctors, яка приймає
+        список лікарів та виводить його на екран
+        у вигляді таблиці. Реалізувати також
+        шапку таблиці.
+
+*/
+
+void Main(string[] args)
+{
+    List<Doctor> doctors = new List<Doctor>();
+    doctors.Add(new Doctor());
+}
---
+++ DataBase_P35/Program.cs
@@ -0,0 +1,77 @@
+﻿using MySql.Data;
+using MySql.Data.MySqlClient;
+using System.Text;
+
+class Program
+{
+    static void CreateTables(MySqlConnection connection)
+    {
+        string query = 
+            "CREATE TABLE IF NOT EXISTS Doctors (" +
+            "   ID INT AUTO_INCREMENT PRIMARY KEY," +
+            "   Name VARCHAR(100)," +
+            "   Premium DOUBLE," +
+            "   Salary DOUBLE" +
+            ");";
+        using(MySqlCommand command = new MySqlCommand(query, connection))
+        {
+            command.ExecuteNonQuery();
+        }
+    }
+
+    static void AddDoctor(MySqlConnection connection, string name, double premium, double salary)
+    {
+        string query =
+            "INSERT INTO Doctors (Name, Premium, Salary) VALUES (@name, @premium, @salary)";
+        using(MySqlCommand command = new MySqlCommand(query, connection))
+        {
+            command.Parameters.AddWithValue("@name", name);
+            command.Parameters.AddWithValue("@premium", premium);
+            command.Parameters.AddWithValue("@salary", salary);
+            command.ExecuteNonQuery();
+        }
+    }
+
+    static void GetDoctors(MySqlConnection connection)
+    {
+        string query = "SELECT * FROM Doctors";
+        using(MySqlCommand command = new MySqlCommand(query, connection))
+        using(MySqlDataReader reader = command.ExecuteReader())
+        {
+            while(reader.Read())
+            {
+                Console.WriteLine($"{reader["ID"], 3} | {reader["Name"], 20} | " +
+                    $"{reader["Premium"], 5} грн. | {reader["Salary"], 6} грн.");
+            }
+        }
+    }
+    
+    public static void Main(string[] args)
+    {
+        Console.OutputEncoding = UTF8Encoding.UTF8;
+        Console.InputEncoding = UTF8Encoding.UTF8;
+        string db_host = "localhost";
+        string db_database = "hospital";
+        string db_user = "root";
+        string db_password = "";
+        
+        string connectionString = $"Server={db_host};Database={db_database};" +
+            $"User ID={db_user};Password={db_password};";
+
+        using (MySqlConnection connection = new MySqlConnection(connectionString))
+        {
+            try
+            { 
+                connection.Open();
+                CreateTables(connection);
+                AddDoctor(connection, "Петров Петро", 150, 1200);
+                GetDoctors(connection);
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            
+        }
+    }
+}
